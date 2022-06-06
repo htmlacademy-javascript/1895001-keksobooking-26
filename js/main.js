4449d050f1e299089300dca9abd2f535baed7a65
RadostevaT <radostevataisia@gmail.com>
2022-06-07 01:45:00 +0500

    Исправления по замечаниям

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomInteger(min, max) { // https://developer.mozilla.org/ Math.random()
-  if (min >= 0 && min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+const getRandomInteger = (min, max) => { // https://developer.mozilla.org/ Math.random()
   if (min > max) {
-    return 'Минимальное значение не может быть больше максимального.';
+    throw new RangeError ('Минимальное значение не может быть больше максимального.');
   }
+
   if (min === max) {
     return min;
   }
-  if (min < 0 || max < 0) {
-    return 'Значения могут быть только положительными либо равными нулю.';
-  }
-  return 'Нет ни одного подходящего числа.';
-}
 
-getRandomInteger(4, 15);
+  if (min < 0 || max < 0) {
+    throw new RangeError ('Значения могут быть только положительными либо равными нулю.');
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomInteger(13, 15);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getTemporaryCoordinate(min, max, precision) {
-  if (min >= 0 && min < max && precision >= 0) {
-    return Number((Math.random() * (max - min + 1)) + min).toFixed(precision);
-  }
+const getRandomFloat = (min, max, precision = 5) => {
   if (min > max) {
-    return 'Минимальное значение не может быть больше максимального.';
+    throw new RangeError ('Минимальное значение не может быть больше максимального.');
   }
+
   if (min === max) {
     return min;
   }
-  if (min < 0 || max < 0) {
-    return 'Значения могут быть только положительными либо равными нулю.';
-  }
-  return 'Нет ни одного подходящего числа.';
-}
 
-getTemporaryCoordinate(1.15, 5.99, 2);
+  if (min < 0 || max < 0) {
+    throw new RangeError ('Значения могут быть только положительными либо равными нулю.');
+  }
+
+  return Number((Math.random() * (max - min)) + min).toFixed(precision);
+};
+
+getRandomFloat(5.15, 5.99);
